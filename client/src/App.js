d06764cd819c4f65f76e460f13d37ba6ccb689fa
Ccruz13 <88520938+Ccruz13@users.noreply.github.com>
2022-02-01 09:16:36 -0500

    added pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,63 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter, HashRouter, Switch, Route } from 'react-router-dom';
-import Nav from './components/Nav/inedx';
-import Home from './components/Home/index';
-import About from './components/About/index';
-import Tools from './components/Tools/index';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
+import Home from './pages/Home';
+// import Detail from './pages/Detail';
+import NoMatch from './pages/NoMatch';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Nav from './components/Nav';
+import { StoreProvider } from './utils/GlobalState';
+// import Success from './pages/Success';
+// import OrderHistory from './pages/OrderHistory';
 
-import './App.css';
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
-const App = () => (
-  <div>
-  <HashRouter>
-      <Nav></Nav>
-      <div className="content">
-          <Switch>
-              <Route exact path="/" component={Home}/>
-              <Route path="/about" component={About}/>
-              <Route path="/tools" component={Tools}/>
-          </Switch>
-      </div>
-  </HashRouter>
-</div>
-);
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              {/* <Route exact path="/success" component={Success} />
+              <Route exact path="/orderHistory" component={OrderHistory} />
+              <Route exact path="/products/:id" component={Detail} /> */}
+              <Route component={NoMatch} />
+            </Switch>
+          </StoreProvider>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
 
 export default App;
--- client/src/components/Nav/inedx.js
+++ client/src/components/Nav/inedx.js
@@ -1,21 +1,58 @@
-import React from 'react';
-import  { NavLink } from "react-router-dom";
-import { Container } from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap';
+import React from "react";
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
 
 function Nav() {
-    return (
-        <Navbar bg="light" variant="light">
-            <Container>
-                <h1>Rent-a-Tool</h1>
-                <ul>
-                    <li><NavLink exact to="/">Home</NavLink></li>
-                    <li><NavLink to="/tools">Tools</NavLink></li>
-                    <li><NavLink to="/about">About</NavLink></li>
-                </ul>
-            </Container>
-        </Navbar>
-    )
+
+  function showNavigation() {
+    if (Auth.loggedIn()) {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/orderHistory">
+              Order History
+            </Link>
+          </li>
+          <li className="mx-1">
+            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+            <a href="/" onClick={() => Auth.logout()}>
+              Logout
+            </a>
+          </li>
+        </ul>
+      );
+    } else {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/signup">
+              Signup
+            </Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/login">
+              Login
+            </Link>
+          </li>
+        </ul>
+      );
+    }
+  }
+
+  return (
+    <header className="flex-row px-1">
+      <h1>
+        <Link to="/">
+          <span role="img" aria-label="wrench">🔧</span>
+            Rent-a-Tool
+        </Link>
+      </h1>
+
+      <nav>
+        {showNavigation()}
+      </nav>
+    </header>
+  );
 }
 
 export default Nav;
